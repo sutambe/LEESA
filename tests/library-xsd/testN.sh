574ad0d5388cb45ceee491b884ccf4e6bd4a12ab
sutambe <sutambe@2826f55e-c080-0410-a89d-be5d81446b12>
2011-04-24 00:16:08 +0000

    Sun Apr 24 00:13:25 UTC 2011  Sumant Tambe  <sutambe@nospam>

--- tests/library-xsd/testN.sh
+++ tests/library-xsd/testN.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in {1..11}
+for i in {1..13}
 do
   touch driver.cxx
-#make CXX=g++45 CXXFLAGS="-Wno-deprecated -D TEST$i -D WITH_LEESA"
-make CXX=g++45 CXXFLAGS="-std=c++0x -Wno-deprecated -D TEST$i -D WITH_LEESA"
+make CXX=g++45 CXXFLAGS="-Wno-deprecated -D TEST$i -D WITH_LEESA"
+#make CXX=g++45 CXXFLAGS="-std=c++0x -Wno-deprecated -D TEST$i -D WITH_LEESA"
   if [ $? -ne 0 ] ; then break ; fi
 done
 
